bf7ccf6af659e636fa3ee8d8b931c79cc72ec5b5
Sanjay Kumar <skumar@sbsc.com>
2018-11-21 23:27:39 +0530

    Form Set

--- src/app/shared/layout/footer.component.ts
+++ src/app/shared/layout/footer.component.ts
@@ -3,7 +3,8 @@ import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'layout-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
 
