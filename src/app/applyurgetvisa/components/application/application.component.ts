b3c31a66fde7a4e024617aea1a1075aaf98e75d0
Giridhar Shukla <giridhars@XEBIAINDIA.LOCAL>
2018-11-21 16:54:57 +0530

    Added First form validations

--- src/app/applyurgetvisa/components/application/application.component.ts
+++ src/app/applyurgetvisa/components/application/application.component.ts
@@ -1,23 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {UserService } from '../../../shared';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserService } from "../../../shared";
 
 @Component({
-  selector: 'application-page',
-  templateUrl: './application.html',
-  styleUrls: ['../../applyvisa.css']
+  selector: "application-page",
+  templateUrl: "./application.html",
+  styleUrls: ["../../applyvisa.css"]
 })
 export class ApplicationComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private userService: UserService
-  ) {}
+  constructor(private router: Router, private userService: UserService) {}
 
-  ngOnInit() {
+  firstForm = {
+    applType: "",
+    pptType: "",
+    nationality: "",
+    portOfArrival: "",
+    dob: null,
+    email: "",
+    email_re: "",
+    journeyDate: null,
+    captcha: "",
+    eTouristvisa_service: false,
+    etouristValue: null,
+    eMedicalValue_service: false,
+    eMedicalValue: null,
+    eBusinessValue_service: false,
+    eBusinessValue: null,
+    eMedicalAttvisa_service: false,
+    eMedicalAttvisaValue: null
+  };
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  ngOnInit() {}
+
+  save() {
+    this.goToNext();
   }
 
-  goToNext (){
+  private goToNext() {
     this.router.navigate(["applyvisa/generaldetails"]);
   }
 
+  get diagnostic() { return JSON.stringify(this.firstForm); }
 }
