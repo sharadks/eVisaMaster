2f5c32c7ea300677976eb08bb6b55ebc9048ec88
Giridhar Shukla <giridhars@XEBIAINDIA.LOCAL>
2018-11-22 15:49:22 +0530

    Added Address details form validations

--- src/app/applyurgetvisa/components/addressdetails/addressdetails.component.ts
+++ src/app/applyurgetvisa/components/addressdetails/addressdetails.component.ts
@@ -17,13 +17,50 @@ export class AddressDetailsComponent implements OnInit {
   ) {}
 
   model = {
-
+    pres_houseNo: '',
+    pres_city: '',
+    pres_country: '',
+    pres_state: '',
+    pres_zip: '',
+    phoneNo: '',
+    mobile: '',
+    differentAddrFlag: false,
+    perma_houseNo: '',
+    perma_city: '',
+    perma_state: '',
+    father_name: '',
+    father_nationality: '',
+    father_prev_nationality: '',
+    father_birth_place: '',
+    father_birth_country: '',
+    mother_name: '',
+    mother_nationality: '',
+    mother_prev_nationality: '',
+    mother_birth_place: '',
+    mother_birth_country: '',
+    applicant_marital_status: '',
+    pakistaniParents: 'NO',
+    pakistaniParentsDetails: '',
+    pres_occupation: '',
+    pres_occupationOthers: '',
+    pres_employer: '',
+    pres_designation: '',
+    pres_address: '',
+    pres_phone: '',
+    prev_occupation: '',
+    prev_occupationOthers: '',
+    militaryPoliceFlag: 'NO',
+    militaryOrg: '',
+    militaryDesignation: '',
+    militaryRank: '',
+    militaryPlaceOfPosting: '',
   };
 
   ngOnInit() {
   }
 
   save(data){
+    debugger
     const formattedData = this.applicationService.createRequestForStep3(data);
     this.apiService
       .post(`${environment.saveApplicationThirdPage}`, formattedData)
