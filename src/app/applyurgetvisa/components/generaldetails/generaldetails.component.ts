2276cfdae280048ac9ecc5dabb0be9cc6df9bbac
Giridhar Shukla <giridhars@XEBIAINDIA.LOCAL>
2018-11-22 12:23:13 +0530

    added general details component validations

--- src/app/applyurgetvisa/components/generaldetails/generaldetails.component.ts
+++ src/app/applyurgetvisa/components/generaldetails/generaldetails.component.ts
@@ -13,10 +13,50 @@ export class GeneralDetailsComponent implements OnInit {
     private userService: UserService
   ) {}
 
+  model = {
+    surname: '',
+    givenName: '',
+    changedName: false,
+    prevGivenName: '',
+    prevSurname: '',
+    gender: '',
+    birthCity: '',
+    birthCountry: '',
+    citizenshipNo: '',
+    religion: '',
+    religionOther: '',
+    identificationMarks: '',
+    eduQualification: '',
+    nationalityByBirth: '',
+    livedFor2Years: '',
+    pptNo: '',
+    placeOfIssue: '',
+    issueDate: '',
+    expiryDate: '',
+    pptHeld: false,
+    prevPassportCountryIssue: '',
+    prevPptICNo: '',
+    prePptDateOfIssue: '',
+    prePptPlaceOfIssue: '',
+    prevPptNationality: ''
+  };
+
   ngOnInit() {
   }
   
   goToNext (){
     this.router.navigate(["applyvisa/addressdetails"]);
+  }
+
+  save() {
+
+  }
+  
+  saveAndExit() {
+
+  }
+  //remove in future
+  get diagnostic() {
+    return JSON.stringify(this.model);
   }
 }
